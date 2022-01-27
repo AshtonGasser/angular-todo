a63c46719cbd9aa86a7344ff71d3dc013c785dfe
Ashton Gasser <ashtongasser@gmail.com>
2022-01-26 22:02:03 -0600

    time for backend setup

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {TASKS} from '../../dummy-tasks'
+import { TaskService } from '../../services/task.service';
 import {Task} from '../../Task'
 @Component({
   selector: 'app-tasks',
@@ -7,10 +7,14 @@ import {Task} from '../../Task'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-tasks: Task[] = TASKS;
-  constructor() { }
+tasks: Task[] = [];
+  constructor(private taskService : TaskService) { }
+
+  //normally you want to use observables here if you're using async data
+  //if you're fetching from a server
 
   ngOnInit(): void {
+    //subscribe is kind of like a promise
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
-
 }
