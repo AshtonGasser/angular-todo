0b5fc4fef25e04c909974688796e885cf05f26e1
Ashton Gasser <ashtongasser@gmail.com>
2022-02-03 16:49:24 -0600

    adding python and slqalchemy, having some hiccups

---
+++ src/entities/tasks.py
@@ -0,0 +1,22 @@
+#import {Task} from '../app/Task.ts' 
+from sqlalchemy import Column, String, Integer, DateTime
+
+from datetime import datetime
+from .entity import Entity, Base
+
+
+class Task(Entity, Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    day = Column(DateTime)
+    description = Column(String)
+    reminder = True
+
+
+    def __init__(self, title, day, description, created_by, reminder):
+        Entity.__init__(self, created_by)
+        self.title = title
+        self.day = day
+        self.description = description
+        self.reminder = reminder
---
+++ src/main.py
@@ -0,0 +1,28 @@
+from .entities.entity import Session, engine, Base
+from .entities.tasks import Task
+
+# generate database schema
+Base.metadata.create_all(engine)
+
+# start session
+session = Session()
+
+# check for existing data
+tasks = session.query(Task).all()
+
+if len(tasks) == 0:
+    python_task = Task("SQLAlchemy Task" , "Create a task", "script")
+    session.add(python_task)
+    session.commit()
+    session.close()
+
+    # reload tasks
+
+    tasks = session.query(Task).all()
+
+    # show existing tasks
+
+    print('### Tasks:')
+    for task in tasks:
+        print(f'({task.id}) {task.title} - {task.day}')
+
