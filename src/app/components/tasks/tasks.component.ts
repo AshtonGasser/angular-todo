90784dc9680af2443f6f3c42b8c3aa5a117638ed
Ashton Gasser <ashtongasser@gmail.com>
2022-02-06 23:27:25 -0600

    crudable, almost

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
-import {Task} from '../../Task'
+import { Task } from '../../Task';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-tasks: Task[] = [];
-  constructor(private taskService : TaskService) { }
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) {}
 
   //normally you want to use observables here if you're using async data
   //if you're fetching from a server
@@ -16,5 +16,21 @@ tasks: Task[] = [];
   ngOnInit(): void {
     //subscribe is kind of like a promise
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id! !== task.id))
+      );
+  }
+  toggleReminder (task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe()
+
+  }
+  addTask(task: Task) {
+    
   }
 }
